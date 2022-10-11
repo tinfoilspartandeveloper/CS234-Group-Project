4c4304ad010754fcd14008010064dabc663cc6cc
tinfoilspartandeveloper <115108442+tinfoilspartandeveloper@users.noreply.github.com>
2022-10-11 13:39:25 -0500

    Update CovidUser.java

--- CovidUser.java
+++ CovidUser.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class CovidUser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
+		CovidInfo info = new CovidInfo();
 		
+		System.out.print("Enter in region: ");
+		System.out.print("Enter in cases: ");
+		System.out.print("Enter in deaths: ");
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("Current region:" + info.getSafetyLevel() );
+		System.out.println("Current cases:" + info.getCases() );
+		System.out.println("Current deaths:" + info.getDeaths() );
+		System.out.println("-----------------------------------------");
+		
+
 	}
 
 }
