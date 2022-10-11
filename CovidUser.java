b2720d0479f056a1480108bf2d6e4b15b03b413c
HyperBitGore <bruiserjoe01@gmail.com>
2022-10-11 16:18:29 -0500

    Update CovidUser.java

--- CovidUser.java
+++ CovidUser.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 
 public class CovidUser {
-
+	private ArrayList<CovidInfo> covid_areas;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
