b00015f25584147449fa001db98e9e402d95da8e
tinfoilspartandeveloper <115108442+tinfoilspartandeveloper@users.noreply.github.com>
2022-10-11 12:53:04 -0500

    Update CovidInfo.java
    Skeleton Code

--- CovidInfo.java
+++ CovidInfo.java
@@ -1,4 +1,20 @@
 
 public class CovidInfo {
+	private int region_id;
+	private int cases;
+	private int deaths;
+	private String region;
+	private int currYear;
+	
+	
 
+	public String getSafetyLevel() {
+		return region;
+	}
+	public void setID() {
+		
+	}
+	public void setRegion(){
+		
+	}
 }
