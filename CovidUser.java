d5e3868f43fc7fd464181fdeba7d32bfc5179bcc
HyperBitGore <bruiserjoe01@gmail.com>
2022-10-12 08:15:29 -0500

    Update CovidUser.java

--- CovidUser.java
+++ CovidUser.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CovidUser {
 	private ArrayList<CovidInfo> covid_areas;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner in = new Scanner (System.in);
+		CovidInfo info = new CovidInfo(0, 10, 5, "Winona");
+
+		System.out.print("Enter in region: ");
+		System.out.print("Enter in cases: ");
+		System.out.print("Enter in deaths: ");
+
+		System.out.println("-----------------------------------------");
+		System.out.println("Current region:" + info.getSafetyLevel() );
+		System.out.println("Current cases:" + info.getCases() );
+		System.out.println("Current deaths:" + info.getDeaths() );
+		System.out.println("-----------------------------------------");
 	}
 
 }
