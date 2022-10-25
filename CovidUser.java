cdd3ff4263147d07b5fc1191d0879fd4831bbb6f
tinfoilspartandeveloper <115108442+tinfoilspartandeveloper@users.noreply.github.com>
2022-10-25 09:54:13 -0500

    Update CovidUser.java

--- CovidUser.java
+++ CovidUser.java
@@ -7,12 +7,21 @@ public class CovidUser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner (System.in);
+<<<<<<< Updated upstream
 		CovidInfo info = new CovidInfo(0, 10, 5, "Winona");
 
 		System.out.print("Enter in region: ");
 		System.out.print("Enter in cases: ");
 		System.out.print("Enter in deaths: ");
 
+=======
+		CovidInfo info = new CovidInfo(0, 0, 0, null);
+		
+		System.out.println("Enter in region: ");
+		System.out.println("Enter in cases: ");
+		System.out.println("Enter in deaths: ");
+		
+>>>>>>> Stashed changes
 		System.out.println("-----------------------------------------");
 		System.out.println("Current region:" + info.getSafetyLevel() );
 		System.out.println("Current cases:" + info.getCases() );
