e0f756c758aa89963aab57581f2340e8fce350c1
HyperBitGore <bruiserjoe01@gmail.com>
2022-11-04 15:29:49 -0500

    Create CovidUserGUI.java

---
+++ CovidUserGUI.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class CovidUserGUI extends JFrame implements ActionListener {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
