17e418e3b3e0bea259fa509f962e9848e9efbe09
tinfoilspartandeveloper <115108442+tinfoilspartandeveloper@users.noreply.github.com>
2022-11-07 15:41:25 -0600

    Update CovidUserGUI.java

--- CovidUserGUI.java
+++ CovidUserGUI.java
@@ -1,3 +1,4 @@
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -5,19 +6,46 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class CovidUserGUI extends JFrame implements ActionListener {
-	private JButton addRegion;
+	private JLabel addRegion;
 	private JComboBox regionlist;
-	private JLabel lbl;
+	private JButton regionButton;
+	private JTextArea txtReport;
 	
 	CovidUserGUI(){
+		setTitle ("Covid Information");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(350, 250);
+		setLayout(new FlowLayout());
+		addRegion = new JLabel("Enter the Region: ");
+		String[] region = {"World","US","WSU"};
+		regionlist = new JComboBox(region);
+		regionButton = new JButton();
+		
+	
+		
+		addRegion = new JLabel();
+		regionButton = new JButton("Report"); 
+		txtReport = new JTextArea("7,25");
 		
 		
+		add(addRegion);
+		add(regionlist);
+		add(regionButton);
+		add(txtReport);
+		
+		setVisible(true);
+		
+		regionButton.addActionListener(this);
+		
+
 	}
 	
 	public static void main(String[] args) {
-		
+		new CovidUserGUI();
 		
 	}
 
