7375ff8f693a5ed0e1b0f3b0e72a5882026b1216
HyperBitGore <bruiserjoe01@gmail.com>
2022-11-07 15:10:32 -0600

    Update CovidUserGUI.java

--- CovidUserGUI.java
+++ CovidUserGUI.java
@@ -1,10 +1,21 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class CovidUserGUI extends JFrame implements ActionListener {
-
+	private JButton addRegion;
+	private JComboBox regionlist;
+	private JLabel lbl;
+	
+	CovidUserGUI(){
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		
 		
@@ -12,7 +23,7 @@ public class CovidUserGUI extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
