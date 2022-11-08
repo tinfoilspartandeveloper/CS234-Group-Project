0c45950a901be76a1991beb6c4b8b8277af2889b
HyperBitGore <bruiserjoe01@gmail.com>
2022-11-07 19:42:18 -0600

    Update CovidUserGUI.java

--- CovidUserGUI.java
+++ CovidUserGUI.java
@@ -1,11 +1,14 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -14,6 +17,7 @@ public class CovidUserGUI extends JFrame implements ActionListener {
 	private JComboBox regionlist;
 	private JButton regionButton;
 	private JTextArea txtReport;
+	public ArrayList<CovidInfo> covid_areas = new ArrayList<CovidInfo>();
 	
 	CovidUserGUI(){
 		setTitle ("Covid Information");
@@ -52,7 +56,27 @@ public class CovidUserGUI extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		
+		if(e.getSource() == regionButton) {
+			JTextField region_in = new JTextField(5);
+			JTextField region_cases = new JTextField(5);
+			JTextField region_deaths = new JTextField(5);
+			
+			JPanel panel = new JPanel();
+			panel.add(new JLabel("Region Name: "));
+			panel.add(region_in);
+			panel.add(new JLabel("Region Cases: "));
+			panel.add(region_cases);
+			panel.add(new JLabel("Region Deaths: "));
+			panel.add(region_deaths);
+			
+			int result = JOptionPane.showConfirmDialog(null, panel, "Enter region info", JOptionPane.OK_CANCEL_OPTION);
+			if(result == JOptionPane.OK_OPTION) {
+				CovidInfo inf = new CovidInfo(covid_areas.size(), Integer.parseInt(region_cases.getText()), Integer.parseInt(region_deaths.getText()), region_in.getText());
+				covid_areas.add(inf);
+			}
+			
+			regionlist.addItem(region_in.getText());
+		}
 	}
 
 }
