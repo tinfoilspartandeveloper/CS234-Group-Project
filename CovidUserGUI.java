12aca0e7ebf94056b2c4ba646e92bb3dd6713c5b
HyperBitGore <bruiserjoe01@gmail.com>
2022-11-21 16:30:41 -0600

    Update CovidUserGUI.java
    finished

--- CovidUserGUI.java
+++ CovidUserGUI.java
@@ -13,38 +13,47 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class CovidUserGUI extends JFrame implements ActionListener {
-	private JLabel addRegion;
 	private JComboBox regionlist;
+	private JButton displayButton;
 	private JButton regionButton;
 	private JTextArea txtReport;
 	public ArrayList<CovidInfo> covid_areas = new ArrayList<CovidInfo>();
+	
+	CovidInfo findRegion(String in) {
+		for(int i = 0; i < covid_areas.size(); i++) {
+			if(covid_areas.get(i).getRegion().equals(in)) {
+				return covid_areas.get(i);
+			}
+		}
+		return null;
+	}
 	
 	CovidUserGUI(){
 		setTitle ("Covid Information");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(350, 250);
 		setLayout(new FlowLayout());
-		addRegion = new JLabel("Enter the Region: ");
-		String[] region = {"World","US","WSU"};
-		regionlist = new JComboBox(region);
-		regionButton = new JButton();
 		
+		//String[] region = {"World","US","WSU"};
+		regionlist = new JComboBox();
+		regionButton = new JButton();
 	
 		
-		addRegion = new JLabel();
-		regionButton = new JButton("Report"); 
-		txtReport = new JTextArea("7,25");
+		displayButton = new JButton("Display Region");
+		regionButton = new JButton("New Region"); 
+		txtReport = new JTextArea(7,25);
 		
 		
-		add(addRegion);
+		
 		add(regionlist);
+		add(displayButton);
 		add(regionButton);
 		add(txtReport);
 		
 		setVisible(true);
 		
 		regionButton.addActionListener(this);
-		
+		displayButton.addActionListener(this);
 
 	}
 	
@@ -76,6 +85,17 @@ public class CovidUserGUI extends JFrame implements ActionListener {
 			}
 			
 			regionlist.addItem(region_in.getText());
+		}else if(e.getSource() == displayButton) {
+			//regionlist.getSelectedItem();
+			if(regionlist.getItemCount() > 0) {
+			String str = regionlist.getSelectedItem().toString();
+			CovidInfo too = findRegion(str);
+			txtReport.setText("");
+			txtReport.append("Current region:" + too.getRegion() + "\n");
+			txtReport.append("Current cases:" + too.getCases() + "\n");
+			txtReport.append("Current deaths:" + too.getDeaths() + "\n");
+			txtReport.append("Current safety level: " + too.getSafetyLevel() + "\n");
+			}
 		}
 	}
 
